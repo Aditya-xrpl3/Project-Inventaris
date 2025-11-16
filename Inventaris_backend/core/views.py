dec2ac793a1886c8833090f3ee4f7a2afd035c3b
Zigoat <143062789+Aditya-xrpl3@users.noreply.github.com>
2025-11-16 11:01:07 +0700

    Update views.py

--- Inventaris_backend/core/views.py
+++ Inventaris_backend/core/views.py
@@ -1,2 +1,18 @@
-from rest_framework import generics, permissions
-# Create your views here.
+# core/views.py
+from rest_framework import generics, permissions, filters
+from .models import Barang, LaporanKerusakan
+from .serializers import BarangSerializer, LaporanKerusakanCreateSerializer
+
+class BarangListView(generics.ListAPIView):
+    queryset = Barang.objects.all()
+    serializer_class = BarangSerializer
+    permission_classes = [permissions.AllowAny]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['nama_barang', 'kode_barang', 'meja__nama_meja', 'kategori__nama_kategori']
+    ordering_fields = ['nama_barang', 'kategori', 'status']
+
+# 🔹 Tini revisinya
+class LaporanKerusakanCreateView(generics.CreateAPIView):
+    queryset = LaporanKerusakan.objects.all()
+    serializer_class = LaporanKerusakanCreateSerializers
+    permission_classes = [permissions.AllowAny]
