f8a4bfbf04301add9fdba36dee1bc2e2561c8d93
Zigoat <143062789+Aditya-xrpl3@users.noreply.github.com>
2025-11-16 11:00:45 +0700

    Update serializers.py

--- Inventaris_backend/core/serializers.py
+++ Inventaris_backend/core/serializers.py
@@ -1,6 +1,34 @@
+# core/serializers.py
 from rest_framework import serializers
-from django.contrib.auth.models import User, Group
-from .models import ...
+from .models import Barang, Kategori, Meja, LaporanKerusakan
 
-# Create your serializers here.
+class KategoriSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Kategori
+        fields = ['nama_kategori']
 
+class MejaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Meja
+        fields = ['nama_meja', 'lokasi']
+
+class BarangSerializer(serializers.ModelSerializer):
+    kategori = serializers.StringRelatedField()
+    meja = serializers.StringRelatedField()
+
+    class Meta:
+        model = Barang
+        fields = [
+            'id',
+            'nama_barang',
+            'kode_barang',
+            'status',
+            'kategori',
+            'meja'
+        ]
+
+# 🔹 ini revisinya
+class LaporanKerusakanCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LaporanKerusakan
+        fields = ['meja', 'deskripsi']
