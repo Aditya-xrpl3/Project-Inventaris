bb94b06553efedc3854b90cfc1b295dfc68fa2de
Zigoat <143062789+Aditya-xrpl3@users.noreply.github.com>
2025-11-16 10:59:53 +0700

    Delete inventaris_backend directory

--- inventaris_backend/core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import BarangListView, LaporanKerusakanCreateView
-
-urlpatterns = [
-    path('barang/', BarangListView.as_view(), name='barang-list'),
-    path('lapor-kerusakan/', LaporanKerusakanCreateView.as_view(), name='lapor-kerusakan'),
-]
